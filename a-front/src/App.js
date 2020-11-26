65d0ddf5fd3e5406b7fb2518387832190317a84a
Jien Huang <huangjien@gmail.com>
2020-11-26 23:03:43 +0000

    runnable

--- a-front/src/App.js
+++ a-front/src/App.js
@@ -1,32 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 import { setupWorker } from 'msw';
+import { useState, useEffect } from 'react';
+import "./tailwind.css"
+import useFetch from 'use-http'
+import Dashboard from './Dashboard'
 
 if (process.env.NODE_ENV === 'development') {
-  const {handlers} = require('./mocks/handlers');
+  const { handlers } = require('./mocks/handlers');
   const worker = setupWorker(...handlers);
   worker.start();
 }
 
 function App() {
+  const [data, setData] = useState([])
+  const { get, post, response, loading, error } = useFetch('http://localhost:3000')
+
+  useEffect(() => {
+    loadInitialData();
+  }, [])
+
+  async function loadInitialData() {
+    const res = await get('/config');
+    if (response.ok) setData(response.json)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Dashboard />
   );
 }
 
 export default App;
+
+
--- a-front/src/mocks/handlers.js
+++ a-front/src/mocks/handlers.js
@@ -2,12 +2,44 @@ import { rest } from 'msw'
 
 var config_data = require('./config_data.json')
 
-handlers = []
+var handlers = []
 function getHandlersFromJson(jsonData) {
 	for(var key of Object.keys(jsonData)) {
-		var el = jsonData[key]
+		const el = jsonData[key]
 		if (el.method === 'get') {
 			handlers.push(rest.get(el.url, (req, res, context) => {
+				return res(
+					context.status(el.status),
+					context.json(el.data)
+				)
+			}))
+		}
+		if (el.method === 'post') {
+			handlers.push(rest.post(el.url, (req, res, context) => {
+				return res(
+					context.status(el.status),
+					context.json(el.data)
+				)
+			}))
+		}
+		if (el.method === 'put') {
+			handlers.push(rest.put(el.url, (req, res, context) => {
+				return res(
+					context.status(el.status),
+					context.json(el.data)
+				)
+			}))
+		}
+		if (el.method === 'delete') {
+			handlers.push(rest.delete(el.url, (req, res, context) => {
+				return res(
+					context.status(el.status),
+					context.json(el.data)
+				)
+			}))
+		}
+		if (el.method === 'head') {
+			handlers.push(rest.head(el.url, (req, res, context) => {
 				return res(
 					context.status(el.status),
 					context.json(el.data)
